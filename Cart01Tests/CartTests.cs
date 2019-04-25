4b5f4f50b7dbf32ea0ba279bfdc52945f2e3a8b1
dave <d2_git@lobban-banff.co.uk>
2019-04-25 09:09:03 +0100

    Gets the total for all items if bought as a single item per sku cart

--- Cart01Tests/CartTests.cs
+++ Cart01Tests/CartTests.cs
@@ -22,5 +22,19 @@ namespace Tests
             var actual = subject.Total;
             Assert.AreEqual(actual, expected);
         }
+
+
+        [TestCase("A99", "B15", 80)]
+        [TestCase("B15", "A99", 80)]
+        [TestCase("C40", "A99", 110)]
+        [TestCase("T34", "A99", 149)]
+        public void Total_SingleItemsOfMultipleSkusScanned_ExpectedPriceReturned(string sku1, string sku2, int expected)
+        {
+            var subject = new Cart();
+            subject.Scan(sku1);
+            subject.Scan(sku2);
+            var actual = subject.Total;
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
