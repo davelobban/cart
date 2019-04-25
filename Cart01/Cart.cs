f542b1acca5645feeb77e9491d61642cf482cffd
dave <d2_git@lobban-banff.co.uk>
2019-04-25 09:06:50 +0100

    Gets the total for all items if bought as a single item cart

--- Cart01/Cart.cs
+++ Cart01/Cart.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cart01
 {
     public class Cart
     {
-        public void Scan(string sku)
+        private IDictionary<string, int> _priceList;
+        private IDictionary<string, int> _scannedSkus= new Dictionary<string, int>();
+        
+        public Cart()
         {
-            
+            _priceList = new Dictionary<string, int>
+            {
+                {"A99", 50},
+                {"B15", 30},
+                {"C40", 60},
+                {"T34", 99},
+            };
         }
 
-        public int Total => 50;
+        public void Scan(string sku)
+        {
+            _scannedSkus.Add(sku,1);
+        }
+
+        public int Total
+        {
+            get
+            {
+                var total = 0;
+                foreach (var sku in _scannedSkus.Keys)
+                {
+                    total += _priceList[sku];
+                }
+
+                return total;
+            }
+        }
     }
 }
--- Cart01Tests/CartTests.cs
+++ Cart01Tests/CartTests.cs
@@ -12,6 +12,9 @@ namespace Tests
         }
 
         [TestCase("A99", 50)]
+        [TestCase("B15", 30)]
+        [TestCase("C40", 60)]
+        [TestCase("T34", 99)]
         public void Total_SingleItemScanned_UnitPriceReturned(string sku, int expected)
         {
             var subject = new Cart();
