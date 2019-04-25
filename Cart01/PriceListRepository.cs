c3a0310cd765d7022a8cc9a54cae7dc07a68c666
dave <d2_git@lobban-banff.co.uk>
2019-04-25 10:30:00 +0100

    Made the repos populate their backing data just once

--- Cart01/PriceListRepository.cs
+++ Cart01/PriceListRepository.cs
@@ -4,19 +4,18 @@ namespace Cart01
 {
     internal class PriceListRepository : IPriceListRepository
     {
+        static Dictionary<string, int> _priceList = new Dictionary<string, int>
+        {
+            {"A99", 50},
+            {"B15", 30},
+            {"C40", 60},
+            {"T34", 99},
+        };
+
         public int GetPriceFor(string sku)
         {
-            return GetPriceList()[sku];
+            return _priceList[sku];
         }
-        static Dictionary<string, int> GetPriceList()
-        {
-            return new Dictionary<string, int>
-            {
-                {"A99", 50},
-                {"B15", 30},
-                {"C40", 60},
-                {"T34", 99},
-            };
-        }
+
     }
 }
--- Cart01/SpecialOfferRepository.cs
+++ Cart01/SpecialOfferRepository.cs
@@ -5,25 +5,22 @@ namespace Cart01
 {
     internal class SpecialOfferRepository : ISpecialOfferRepository
     {
+
+        static List<SpecialOffer>  _specialOffers = new List<SpecialOffer>
+        {
+            new SpecialOffer("A99",3,130),
+            new SpecialOffer("B15",2,45),
+        };
         public (bool Found, SpecialOffer Offer) GetOfferFor(string sku)
         {
-            //return GetSpecialOffers().FirstOrDefault(s => s.Sku == sku);
-            var specialOfferFound = GetSpecialOffers().Any(s => s.Sku == sku);
+            var specialOfferFound = _specialOffers.Any(s => s.Sku == sku);
             if (specialOfferFound)
             {
-                return (true, GetSpecialOffers().First(s => s.Sku == sku));
+                return (true, _specialOffers.First(s => s.Sku == sku));
             }
 
             return (false, new SpecialOffer("",0,0));
         }
 
-        internal static List<SpecialOffer> GetSpecialOffers()
-        {
-            return new List<SpecialOffer>
-            {
-                new SpecialOffer("A99",3,130),
-                new SpecialOffer("B15",2,45),
-            };
-        }
     }
 }
